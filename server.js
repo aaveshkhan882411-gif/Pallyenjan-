7da2619cd58e1c99955911ef9a56ac9bbf8bd2f4
aaveshkhan882411-gif <aaveshkhan.882411@gmail.com>
2026-09-07 23:02:29 +0530

    Change static file serving and index file path

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(__dirname));
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -21,7 +21,7 @@ app.get('/api/health', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
